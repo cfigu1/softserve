5b51037772e6e81f7ff615b8dc02e80726ae2a42
Figiel, Damian <figield@ryanair.com>
2019-10-21 18:29:40 +0200

    server added

--- ryanair/index.js
+++ ryanair/index.js
@@ -1,7 +1,8 @@
 const puppeteer = require('puppeteer');
+const http = require('http');
 const website = 'https://www.ryanair.com/ie/en/trip/flights/select?ADT=1&TEEN=0&CHD=0&INF=0&DateOut=2020-05-01&DateIn=&Origin=STN&Destination=DUB&isConnectedFlight=false&RoundTrip=false&Discount=0&tpAdults=1&tpTeens=0&tpChildren=0&tpInfants=0&tpStartDate=2020-05-01&tpEndDate=&tpOriginIata=STN&tpDestinationIata=DUB&tpIsConnectedFlight=false&tpIsReturn=false&tpDiscount=0';
 
-(async () => {
+async function getData() {
   const browser = await puppeteer.launch({ headless: false, args: ["--ash-host-window-bounds=1920x1080", "--window-size=1920,1048", "--window-position=0,0"] });
   const page = await browser.newPage();
   await page.setViewport({width: 1600, height: 900});
@@ -24,11 +25,14 @@ const website = 'https://www.ryanair.com/ie/en/trip/flights/select?ADT=1&TEEN=0&
       flightNumber,
       flightPrice,
     };
-
-    return JSON.stringify(flightsData);
+    return flightsData;
   });
+  return JSON.stringify(await Promise.all(flightCards));
+};
 
-  console.log(await Promise.all(flightCards));
-  
-})();
-
+const server = http.createServer(async (request, response) => {
+  const webData = await getData(); 
+  response.writeHead(200, { 'Content-Type': 'application/json' });
+  response.end(webData);
+});
+server.listen(3000);
